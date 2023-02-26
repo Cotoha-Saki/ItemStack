5bc9c3cd07d733bd05d04567a659763ef3100af3
sana <sana.programming.04@gmail.com>
2023-02-26 21:22:30 +0900

    Initial commit

---
+++ src/main/java/com/YanaseSana/ItemStock/Main/ItemStock.java
@@ -0,0 +1,9 @@
+package com.YanaseSana.ItemStock.Main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("itemstock")
+public class ItemStock {
+
+    public static final String MOD_ID = "itemstock";
+}
